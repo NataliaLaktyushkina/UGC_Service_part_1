d50b775485af7a4591bea3deb0e8152c275c1d17
NatalyaLaktyushkina <tigerclow@mail.ru>
2022-08-01 01:51:27 +0300

    api: laucnh

---
+++ fast_api/main.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+from core.config import settings
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    docs_url='/api/openapi',
+    openapi_url='/api/openapi.json',
+    default_response_class=ORJSONResponse,
+)
+
+if __name__ == '__main__':
+    uvicorn.run(
+        'main:app',
+        host='0.0.0.0',
+        port=8100,
+    )
---
+++ fast_api/src/core/config.py
@@ -0,0 +1,34 @@
+import os
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = os.getenv('PROJECT_NAME', 'UGC_API')
+
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+
+class PromSettings(Settings):
+    pass
+
+class DevSettings(Settings):
+    pass
+
+def get_settings():
+    environment = os.getenv('ENVIRONMENT')
+    if environment == 'prom':
+        return get_prom_settings()
+    else:
+        return get_dev_settings()
+
+
+def get_prom_settings():
+    return PromSettings()
+
+
+def get_dev_settings():
+    return DevSettings()
+
+settings = get_settings()
